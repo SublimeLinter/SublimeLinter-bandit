c293da4ab9b20147540310c853980fbca984aff5
Koen Lageveen <koen.lageveen@me.com>
2018-03-25 19:58:55 +0200

    clean up things that don't work in SL4

--- linter.py
+++ linter.py
@@ -1,27 +1,8 @@
-#
-# linter.py
-# Linter for SublimeLinter, a code checking framework for Sublime Text 3
-#
-# Written by Eric Brown
-# Copyright (c) 2016 Eric Brown
-#
-# License: MIT
-#
-
-"""This module exports the Bandit plugin class."""
-
-from SublimeLinter.lint import PythonLinter
-from SublimeLinter.lint import util
+from SublimeLinter.lint import PythonLinter, util
 
 
 class Bandit(PythonLinter):
-    """Provides an interface to bandit."""
-
-    syntax = 'python'
-    cmd = ('bandit@python', '*', '-n', '1', '-f', 'txt', '-')
-    version_args = '--version'
-    version_re = r'^bandit\s(?P<version>\d+.\d+.\d+)'
-    version_requirement = '>= 1.3.0'
+    cmd = ('bandit@python', '${args}', '-n', '1', '-f', 'txt', '-')
     regex = (
         r'^>>\sIssue:\s\[(?P<code>[B]\d+):.+\]\s(?P<message>.+)$\r?\n'
         r'^.*Severity:\s(?:(?P<error>High)|(?P<warning>(Medium|Low))).*$\r?\n'
@@ -31,7 +12,5 @@ class Bandit(PythonLinter):
     error_stream = util.STREAM_BOTH
     config_file = ('--ini', '.bandit')
     defaults = {
-        '--tests,': '',
-        '--skips,': '',
+        'selector': 'source.python'
     }
-    inline_overrides = ('tests', 'skips')
